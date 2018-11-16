01fc957f01fcb526fceb6597cb1e6e7603d58afe
Héloïse Coulon <lolocoul@orange.fr>
2018-11-16 16:42:04 +0100

    Test MongoDB

--- express_webapp/test/MusiqueTest.js
+++ express_webapp/test/MusiqueTest.js
@@ -2,13 +2,13 @@ var expect = require('chai').expect;
 var database = require('../bin/db-connection');
 
 describe('music', function() {
-    it('should be invalid if titre is empty', function(done) {
+    it('should be invalid if titre, cheminMP3, duree, listePoints are empty', function(done) {
         var m = new database.Musique();
 
         m.validate(function(err) {
             expect(err.errors.titre).to.exist;
             expect(err.errors.cheminMP3).to.exist;
-            expect(err.errors.artiste).to.exist;
+            expect(err.errors.duree).to.exist;
             done();
         });
     });
