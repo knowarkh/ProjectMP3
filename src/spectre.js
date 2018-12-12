8f9c4eb8aeaf1df2d056ce9d5b62655a36c45bce
AlexAmemoutou <alex.amemoutou@gmail.com>
2018-12-12 08:37:43 +0100

    inversion des barres du spectre

--- src/spectre.js
+++ src/spectre.js
@@ -1,27 +1,35 @@
 function createWaveForm() {
-    var spectre = document.getElementById("spectre");
-    var svgns = "http://www.w3.org/2000/svg";
-    var newSVG = document.createElementNS(svgns, "svg");
+    let spectre = document.getElementsByClassName("waveform")[0];
+    let svgns = "http://www.w3.org/2000/svg";
+    let newSVG = document.createElementNS(svgns, "svg");
+
+    let width_wave = spectre.clientWidth;
+    let height_wave = spectre.clientHeight;
+
+    let x_bar = 0;
+    let y_bar = 0;
+    let width_bar = 5;
+    let height_bar = 0;
+
     newSVG.setAttribute("xmlns", svgns);
-    newSVG.setAttribute("width", "1400");
-    newSVG.setAttribute("height", "800");
+    //Set the rigth size of the waveform
 
-    let x = 0;
-    let y = 0;
-    let width = 3;
-    let height = 0;
+    newSVG.setAttribute("width", width_wave);
+    newSVG.setAttribute("height", height_wave);
 
+    //Create and add every string of the waveform
 
     for (var i = 0; i<400; i++){
         var newRect = document.createElementNS(svgns, "rect");
-        height = getRandomInt(200);
-        newRect.setAttributeNS(null, "x", x);
-        newRect.setAttributeNS(null, "y", y);
-        newRect.setAttributeNS(null, "width", ""+width);
-        newRect.setAttributeNS(null, "height", ""+height);
+        height_bar = getRandomInt(height_wave);
+        y_bar = height_wave - height_bar;
+        newRect.setAttributeNS(null, "x", x_bar);
+        newRect.setAttributeNS(null, "y", y_bar );
+        newRect.setAttributeNS(null, "width", ""+ width_bar);
+        newRect.setAttributeNS(null, "height", ""+ height_bar);
         newRect.setAttributeNS(null, "rx", "5");
         newRect.setAttributeNS(null, "style", "fill:#1d9fcc; stroke:#cc7c29");
-        x+=width;
+        x_bar+=width_bar;
         newSVG.appendChild(newRect);
     }
     spectre.appendChild(newSVG);
