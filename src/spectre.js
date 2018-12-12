75568a4ede225f8d61728ce90be3f634fd3eb0c1
AlexAmemoutou <alex.amemoutou@gmail.com>
2018-12-12 09:40:48 +0100

    ajout du reflect

--- src/spectre.js
+++ src/spectre.js
@@ -1,38 +1,70 @@
 function createWaveForm() {
     let spectre = document.getElementsByClassName("waveform")[0];
     let svgns = "http://www.w3.org/2000/svg";
-    let newSVG = document.createElementNS(svgns, "svg");
 
-    let width_wave = spectre.clientWidth;
-    let height_wave = spectre.clientHeight;
+    let primarySVG = document.createElementNS(svgns, "svg");
+    let reflectSVG = document.createElementNS(svgns, "svg");
+
+    let width_primaryWave = spectre.clientWidth;
+    let height_primaryWave = Math.round(spectre.clientHeight * 2/3);
+
+    let width_reflectWave = width_primaryWave;
+    let height_reflectWave = Math.round(spectre.clientHeight/3);
 
     let x_bar = 0;
     let y_bar = 0;
-    let width_bar = 5;
+    let width_bar = 2;
     let height_bar = 0;
 
-    newSVG.setAttribute("xmlns", svgns);
-    //Set the rigth size of the waveform
+    primarySVG.setAttribute("xmlns", svgns);
+    //Set the rigth size of the primary waveform
 
-    newSVG.setAttribute("width", width_wave);
-    newSVG.setAttribute("height", height_wave);
+    primarySVG.setAttribute("width", width_primaryWave);
+    primarySVG.setAttribute("height", height_primaryWave);
 
-    //Create and add every string of the waveform
+    //Create and add every string of the primary waveform
 
     for (var i = 0; i<400; i++){
         var newRect = document.createElementNS(svgns, "rect");
-        height_bar = getRandomInt(height_wave);
-        y_bar = height_wave - height_bar;
+        height_bar = getRandomInt(height_primaryWave);
+        y_bar = height_primaryWave - height_bar;
         newRect.setAttributeNS(null, "x", x_bar);
         newRect.setAttributeNS(null, "y", y_bar );
         newRect.setAttributeNS(null, "width", ""+ width_bar);
         newRect.setAttributeNS(null, "height", ""+ height_bar);
-        newRect.setAttributeNS(null, "rx", "5");
-        newRect.setAttributeNS(null, "style", "fill:#1d9fcc; stroke:#cc7c29");
+        newRect.setAttributeNS(null, "rx", "0");
+        newRect.setAttributeNS(null, "style", "fill:#333333; stroke:#CCCCCC; stroke-width:0.5");
         x_bar+=width_bar;
-        newSVG.appendChild(newRect);
+        primarySVG.appendChild(newRect);
     }
-    spectre.appendChild(newSVG);
+    spectre.appendChild(primarySVG);
+
+    x_bar = 0;
+    y_bar = 0;
+
+    reflectSVG.setAttribute("xmlns", svgns);
+    //Set the rigth size of the reflect waveform
+
+    reflectSVG.setAttribute("width", width_reflectWave);
+    reflectSVG.setAttribute("height", height_reflectWave);
+
+    //Create and add every string of the reflect waveform
+
+    for (var i = 0; i<400; i++){
+        var newRect = document.createElementNS(svgns, "rect");
+        height_bar = getRandomInt(height_reflectWave * 2 / 3);
+        //y_bar = height_reflectWave - height_bar;
+        newRect.setAttributeNS(null, "x", x_bar);
+        newRect.setAttributeNS(null, "y", y_bar );
+        newRect.setAttributeNS(null, "width", ""+ width_bar);
+        newRect.setAttributeNS(null, "height", ""+ height_bar);
+        newRect.setAttributeNS(null, "rx", "0");
+        newRect.setAttributeNS(null, "style", "fill:#888888; stroke:#CCCCCC; stroke-width:0.5");
+        x_bar+=width_bar;
+        reflectSVG.appendChild(newRect);
+    }
+    spectre.appendChild(reflectSVG);
+
 
 }
 
