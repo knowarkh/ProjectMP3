473f5267bdd8d61c13f2578ca4667db6e212595a
AlexAmemoutou <alex.amemoutou@gmail.com>
2019-01-07 11:02:04 +0100

    Commun - add some documentations

--- express_webapp/public/javascripts/Utils.js
+++ express_webapp/public/javascripts/Utils.js
@@ -1,12 +1,19 @@
-
-
-
+/**
+ * Transform the duration in a number of seconds to a readable text
+ * @param duree {int} - number of seconds
+ * @returns {string} - Text version of the time
+ */
 function secondsToReadableTime(duree){
     var minutes = Math.floor(duree / 60);
     var seconds = duree - minutes * 60;
     return minutes + ":" + str_pad_left(seconds,'0',2);
 }
 
+/**
+ * Transform the duration in a number of milliseconds to a readable text
+ * @param duree {int} - number of milliseconds
+ * @returns {string} - Text version of the time
+ */
 function miliSecondsToReadableTime(duree){
   return secondsToReadableTime(Math.floor(duree/1000));
 }
