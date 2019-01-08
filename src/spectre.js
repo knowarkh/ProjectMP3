76133a071fe401040cc6c4e55e2caf54ce6d8fb6
HeloiseCoulon <lolocoul@orange.fr>
2019-01-08 11:56:23 +0100

    Volume - Reste a plug sur node

--- src/spectre.js
+++ src/spectre.js
@@ -1,10 +1,16 @@
 let volume_button = document.querySelector(".audioplayer .controls .volume .volume_button");
 let volume_span = document.querySelector(".audioplayer .controls .volume");
 let volume_input = document.querySelector('input[type=range].volume-input-range');
+let volume_slider = document.querySelector('audioplayer .controls .volume .volume_slider');
 
 volume_button.addEventListener("click", mute);
-volume_button.addEventListener("mouseover", volumeMouseOver);
-volume_button.addEventListener("mouseout", volumeMouseOut);
+volume_input.addEventListener('input', targetVolume);
+
+volume_span.addEventListener("mouseover", volumeMouseOver);
+volume_span.addEventListener("mouseout", volumeMouseOut);
+
+// volume_slider.addEventListener("mouseover", volumeMouseOver);
+// volume_slider.addEventListener("mouseout", volumeMouseOut);
 
 function mute(){
     if(volume_button.classList.contains('volume-on')) {
@@ -26,10 +32,10 @@ function volumeMouseOut(){
 }
 
 
-volume_input.addEventListener('input', function(e){
+function targetVolume(e){
   var min = e.target.min,
       max = e.target.max,
       val = e.target.value;
 
   e.target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
-});
+}
