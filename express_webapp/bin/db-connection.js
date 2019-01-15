6471f2ee2ffd051d1ea581760dbfc0bf56775065
Héloïse Coulon <lolocoul@orange.fr>
2019-01-15 17:10:28 +0100

    CSS - Player - Fin du gros CSS

--- express_webapp/bin/db-connection.js
+++ express_webapp/bin/db-connection.js
@@ -9,7 +9,6 @@ mongoose.connect(urlmongo, options);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
 db.once('open', function () {
-    // console.log("Connexion à la base OK");
 });
 
 var musicSchema = mongoose.Schema({
