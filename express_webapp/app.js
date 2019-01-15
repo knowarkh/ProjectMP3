9a51d38a9ab8c344d87da037275a39f954a9d066
AlexAmemoutou <alex.amemoutou@gmail.com>
2019-01-15 15:37:12 +0100

    Path - update the path and Postman

--- express_webapp/app.js
+++ express_webapp/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var musicRouter = require('./routes/music');
+var playlistRouter = require('./routes/playlist');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/music', musicRouter);
+app.use('/playlist', playlistRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express_webapp/routes/music.js
+++ express_webapp/routes/music.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var database = require('../bin/db-connection');
+var Music = require('../bin/db-connection').database.Musique;
 var path = require('path');
 
 /* GET music listing. */
@@ -14,10 +14,9 @@ router.get('/embed', function(req, res, next) {
 
 // ========== GET ==========
 
-/**         Music        */
 //Find all
-router.get('/findAllMusic', function(req,res){
-	database.database.Musique.find(function(err, music){
+router.get('/findAll', function(req,res){
+	Music.find(function(err, music){
         if (err){
             res.send(err);
         }
@@ -26,8 +25,8 @@ router.get('/findAllMusic', function(req,res){
 });
 
 // Find by ID
-router.get('/find/music/id/:id', function(req,res){
-	database.database.Musique.find({id: req.params.id}, function(err, music) {
+router.get('/find/id/:id', function(req,res){
+	Music.find({id: req.params.id}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -37,9 +36,9 @@ router.get('/find/music/id/:id', function(req,res){
 
 // Find by titre
 
-router.get('/find/music/title/:title', function(req,res){
+router.get('/find/title/:title', function(req,res){
     let title = new RegExp(req.params.title);
-	database.database.Musique.find({titre: { $regex : title}}, function(err, music) {
+	Music.find({titre: { $regex : title}}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -50,7 +49,7 @@ router.get('/find/music/title/:title', function(req,res){
 // Find by artiste
 
 router.get('/find/artiste/:artiste', function(req,res){
-	database.database.Musique.find({artiste: req.params.artiste}, function(err, music) {
+	Music.find({artiste: req.params.artiste}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -61,7 +60,7 @@ router.get('/find/artiste/:artiste', function(req,res){
 // Find by album
 
 router.get('/find/album/:album', function(req,res){
-	database.database.Musique.find({album: req.params.album}, function(err, music) {
+	Music.find({album: req.params.album}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -72,7 +71,7 @@ router.get('/find/album/:album', function(req,res){
 // Find by genre
 
 router.get('/find/genre/:genre', function(req,res){
-	database.database.Musique.find({genre: req.params.genre}, function(err, music) {
+	Music.find({genre: req.params.genre}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -83,7 +82,7 @@ router.get('/find/genre/:genre', function(req,res){
 // Find by annee
 
 router.get('/find/annee/:annee', function(req,res){
-	database.database.Musique.find({annee: req.params.annee}, function(err, music) {
+	Music.find({annee: req.params.annee}, function(err, music) {
         if (err) {
             res.send(err);
         }
@@ -91,47 +90,12 @@ router.get('/find/annee/:annee', function(req,res){
     });
 });
 
-/**         Playlist         */
 
-router.get('/find/playlist/id/:id', function(req, res){
-    database.database.Playlist.findOne({id: req.params.id}, function(err,playlist){
-        if(err){
-            res.send(err);
-        }
-        let result = {};
-        let listIdMusicToSearch = playlist.listIdMusic;
-        let numberOfQueryFinished = 0;
-
-        for(let i = 0; i < listIdMusicToSearch.length; i++){
-            database.database.Musique.findOne({id:listIdMusicToSearch[i]},function(err,music){
-                result[listIdMusicToSearch[i]] = music;
-
-                //Use a counter to know if all query is finished
-                numberOfQueryFinished++;
-
-                if(numberOfQueryFinished === listIdMusicToSearch.length){
-                    res.json(result);
-                }
-            });
-
-        }
-    });
-});
-
-router.get('/find/playlist/name/:name', function(req,res){
-    let name = new RegExp(req.params.name);
-    database.database.Playlist.find({name: {$regex : name}}, function(err, playlists){
-        if(err)
-            res.send(err);
-        res.json(playlists);
-    });
-});
 
 // ========== POST ==========
-/**         Music        */
 
 router.post('/add', function(req,res){
-    var music = new database.database.Musique();
+    var music = new Music();
 
     music.id = req.body.id;
     music.titre = req.body.titre;
@@ -156,28 +120,12 @@ router.post('/add', function(req,res){
     });
 });
 
-/**         Playlist         */
 
-router.post('/addPlaylist', function(req,res){
-    var playlist = new database.database.Playlist();
-
-    playlist.id = req.body.id;
-    playlist.name = req.body.name;
-    playlist.listIdMusic = req.body.listIdMusic;
-
-    playlist.save(function(err){
-        if(err) {
-            res.send(err);
-        }
-        res.send({message : 'Ajout de la Playlist réussit !'})
-    });
-});
 // ========== PUT ==========
-/**         Music        */
 
 
-router.put('/maj/music/:id', function(req, res) {
-    database.database.Musique.update({id: req.params.id}, req.body, function(err, status) {
+router.put('/maj/:id', function(req, res) {
+    Music.update({id: req.params.id}, req.body, function(err, status) {
         if (err) {
             res.send(err);
         }
@@ -187,8 +135,8 @@ router.put('/maj/music/:id', function(req, res) {
 
 // addLike
 
-router.put('/maj/music/like/:id', function(req, res) {
-    database.database.Musique.update({id: req.params.id}, {$inc:{nbLike : 1}}, function(err, status) {
+router.put('/maj/like/:id', function(req, res) {
+    Music.update({id: req.params.id}, {$inc:{nbLike : 1}}, function(err, status) {
         if (err) {
             res.send(err);
         }
@@ -199,8 +147,8 @@ router.put('/maj/music/like/:id', function(req, res) {
 
 // removeLike
 
-router.put('/maj/music/removeLike/:id', function(req, res) {
-    database.database.Musique.update({id: req.params.id}, {$inc:{nbLike : -1}}, function(err, status) {
+router.put('/maj/removeLike/:id', function(req, res) {
+    Music.update({id: req.params.id}, {$inc:{nbLike : -1}}, function(err, status) {
         if (err) {
             res.send(err);
         }
@@ -211,8 +159,8 @@ router.put('/maj/music/removeLike/:id', function(req, res) {
 
 // addViews
 
-router.put('/maj/music/views/:id', function(req, res) {
-    database.database.Musique.update({id: req.params.id}, {$inc:{nbEcoute : 1}}, function(err, status) {
+router.put('/maj/views/:id', function(req, res) {
+    Music.update({id: req.params.id}, {$inc:{nbEcoute : 1}}, function(err, status) {
         if (err) {
             res.send(err);
         }
@@ -222,8 +170,8 @@ router.put('/maj/music/views/:id', function(req, res) {
 
 // removeViews
 
-router.put('/maj/music/removeViews/:id', function(req, res) {
-    database.database.Musique.update({id: req.params.id}, {$inc:{nbEcoute : -1}}, function(err, status) {
+router.put('/maj/removeViews/:id', function(req, res) {
+    Music.update({id: req.params.id}, {$inc:{nbEcoute : -1}}, function(err, status) {
         if (err) {
             res.send(err);
         }
@@ -231,53 +179,16 @@ router.put('/maj/music/removeViews/:id', function(req, res) {
     });
 });
 
-/**         Playlist         */
-
-router.put('/maj/playlist/add/:id', function(req,res){
-    database.database.Musique.findOne({id : req.body.id}, function(err, music){
-
-        database.database.Playlist.update({id : req.params.id},{$addToSet: {listIdMusic : music.id}}, function(err, status){
-            res.json(status);
-        });
-    });
-
-});
-
-router.put('/maj/playlist/remove/:id', function(req,res){
-    database.database.Musique.findOne({id : req.body.id}, function(err, music){
-
-        database.database.Playlist.update({id : req.params.id},{$pull: { listIdMusic: music.id}}, function(err, status){
-            res.json(status);
-        });
-    });
-
-});
-
 // ========== DELETE ==========
-/**         Music        */
 
-
-
-router.delete('/remove/music/:id', function(req, res) {
-    database.database.Musique.remove({id: req.params.id}, function(err, music){
+router.delete('/remove/:id', function(req, res) {
+    Music.remove({id: req.params.id}, function(err, music){
         if (err){
             res.send(err);
         }
         res.json({message:"Musique supprimée"});
     });
 });
-
-/**         Playlist         */
-
-router.delete('/remove/playlist/:id', function(req, res) {
-    database.database.Playlist.remove({id: req.params.id}, function(err, music){
-        if (err){
-            res.send(err);
-        }
-        res.json({message:"Playlist supprimée"});
-    });
-});
-
 
 
 module.exports = router;
